bbb06429caf1763ee27e070ecf6e070d43567bd8
Cstarmand <145289648+Cstarmand@users.noreply.github.com>
2024-08-27 18:27:34 -0400

    Serializer and Views

--- repo/directory/directory/views.py
+++ repo/directory/directory/views.py
@@ -1,3 +1,18 @@
 from django.http import JsonResponse
 from .models import CSPANdata
 from .serializer import CSPANSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+@api_view(['GET'])
+
+def getData(request, format=None):
+    if request.method == 'GET':
+        #filler so we don't have an error
+        print('test')
+
+@api_view(['PUT'])
+
+def postSummary(request, format=None):
+    #filler so we don't have an error
+    print("also test")
