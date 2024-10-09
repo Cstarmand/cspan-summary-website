6147a1ae02884b6bc3c619f4ea5ffc1ed8736887
Cstarmand <145289648+Cstarmand@users.noreply.github.com>
2024-10-08 20:00:36 -0400

    API
    claude api works but we'll need to put in funds, c-span api is not working, resutling in forbidden error (likely problem with my code, but possibly api key issues)

--- repo/directory/directory/CSPAN.py
+++ repo/directory/directory/CSPAN.py
@@ -10,12 +10,16 @@ from datetime import datetime
 #from .models import CSPANdata
 
 val = datetime.now().date()
-api_url = f"https://api.c-spanarchives.org/2.0/mentions?query=speaker&limit=&personid=&date={val}&maxdate=&mindate=&page=&videotype="
 api_key = "0j2wWr10819hVgK4DT8LSata-j82U84y9vafA4YX"
-auth = HTTPBasicAuth('apikey', api_key)
-response = requests.request("get", api_url, headers=None, auth=auth)
+api_url = f"https://api.c-spanarchives.org/2.0/mentions?query=speaker&limit=&personid=&date={val}&maxdate=&mindate=&page=&videotype=/get"
+auth_ = HTTPBasicAuth('apikey', api_key)
+response = requests.get(api_url, headers={"Authentication": f"Basic {api_key}"})
 
 if response.status_code == 200:
     data = response.json()
+    print(data)
 else:
     data = response.status_code
+    print(data)
+
+# messed around with program, kept getting 403 error (forbidden) no matter how I changed headers and auth
--- repo/directory/directory/claude.py
+++ repo/directory/directory/claude.py
@@ -1,19 +1,27 @@
 import requests
 import json
-from .models import CSPANdata
-import views
+import anthropic
+#from .models import CSPANdata
+#import views
 
 ENDPOINT = 'https://api.anthropic.com/v1/claude-3.5-sonnet'
 # fyi current claude account is on free, 429 error is out of tokens
-KEY = 'sk-ant-api03-yjWtPheKJUMc8nt_NxmFayd17CVhjwaqRTyNjHnq9I0Nn1pwoByOfLrkswX7GiHiRwuEyRi9MaX9hTEja7dESQ-GnJ-RAAA'
+KEY = f"sk-ant-api03-yjWtPheKJUMc8nt_NxmFayd17CVhjwaqRTyNjHnq9I0Nn1pwoByOfLrkswX7GiHiRwuEyRi9MaX9hTEja7dESQ-GnJ-RAAA"
 DATA = 'our-pulled-data'
 
-headers = {"Content-Type":"application/json", "Authentication": f"Bearer {KEY}"}
-data = {"prompt": f"Summarize the following meeting, using key terminology: {DATA}", "max_tokens_to_sample": 1024}
+client = anthropic.Anthropic(api_key=KEY)
 
-response = requests.post(ENDPOINT, headers=headers, data = json.dumps(data))
+message = client.messages.create(model="claude-3-5-sonnet-20240620", max_tokens=1024, temperature=0, system="You are a summary tool, whose job is to summarize a government meeting, keeping key terminology and topics, and writing a shortened and readable summary which is understandable to the average citizen.", messages = [{"role":"user", "content":[{"type":"text", "text":f"{DATA}"}]}])
+message.content
 
-if response.status_code == 200:
-    result = response.json()
-else: 
-    result = f"Error: {response.status_code}"
+# headers = {"Content-Type":"application/json", "Authentication": f"Bearer {KEY}"}
+# data = {"prompt": f"Summarize the following meeting, using key terminology: {DATA}", "max_tokens_to_sample": 1024}
+
+# response = requests.post(ENDPOINT, headers=headers, data = json.dumps(data))
+
+# if response.status_code == 200:
+#     result = response.json()
+#     print(result)
+# else: 
+#     result = f"Error: {response.status_code}"
+#     print(result)
