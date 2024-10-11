497ce05b5ac598ee0305b1caa3f53f9491939484
KokaKolaPeninsula <thomsjake1@gmail.com>
2024-10-11 13:24:54 -0400

    Create transcript_retrieval_functions.py
    This uses a webscraper so if you would want to get all 700 house transcripts it would take a very long time because the delay is 2 seconds per page (so around 1400 seconds total).

---
+++ repo/directory/directory/transcript_retrieval_functions.py
@@ -0,0 +1,78 @@
+#importing required modules
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+import time
+import json
+
+#this gets the transcript for individual sessions
+def get_transcript(url):
+    time.sleep(2)
+    #Initialize selenium using chrome and get the content into the driver
+    driver = webdriver.Chrome()
+    driver.get(url)
+
+    #makes it into a beautiful soup and then extracts from the <pre> tag the transcript
+    soup = BeautifulSoup(driver.page_source, features="lxml")
+
+    raw_transcript = soup.find_all('pre')[0]
+
+    return raw_transcript.text
+
+#gets a list of congressional hearings from a given page
+def get_session_list(url="https://www.congress.gov/house-hearing-transcripts/118th-congress"):
+    #Initialize selenium using chrome and get the content into the driver
+    driver = webdriver.Chrome()
+    driver.get(url)
+    
+    #makes it into a beautiful soup and then extracts from the <pre> tag the transcript
+    soup = BeautifulSoup(driver.page_source, features="lxml")
+    
+    tableRows = [tag for tag in soup.find_all('tr')]
+
+    return tableRows
+
+#turns the list of sessions into a list of dictionaries
+def dictify_session_list(tableRows: BeautifulSoup):
+    events = []
+
+    for i in tableRows[1::]:
+        event = {}
+        link = 'https://www.congress.gov' + i.find_all('a')[0]['href']
+        if link[-5::] != '/text':
+            link += '/text'
+        event['link'] = link
+        tags = i.find_all('td')
+        event['date'] = tags[1].text
+        event['title'] = tags[2].text
+        event['id'] = int(tags[0]['data-text'])
+        committee = tags[3].find_all('a')
+        if len(committee) > 0:
+            event['committee'] = committee[0].text
+        else:
+            event['committee'] = 'Non Specific'
+        events.append(event)
+    return events
+
+#returns of a list of event dictionaries with their transcript added
+def add_transcripts(events: list):
+    for i in range(len(events)):
+        try:
+            events[i]['transcript'] = get_transcript(events[i]['link'])
+        except:
+            events[i]['transcript'] = ''
+            print(f'error at index {i} in events')
+
+
+#little example of what it can do (only runs as the main file)
+if __name__ == "__main__":
+    house_sessions = get_session_list()
+    events = dictify_session_list(house_sessions)
+
+    sub_events = events[0:5]
+
+    transcript_and_events = add_transcripts(sub_events)
+
+    events_json = json.dumps(transcript_and_events)
+
+    print(events_json[0])
