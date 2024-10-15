8bcfbc9dbaee77e19cc54c8e7f4b5b1a82392998
Cstarmand <145289648+Cstarmand@users.noreply.github.com>
2024-10-14 21:53:46 -0400

    Updated Requirements.py
    Transcript functions working but denied access.

--- repo/directory/directory/CSPAN.py
+++ repo/directory/directory/CSPAN.py
@@ -10,10 +10,10 @@ from datetime import datetime
 #from .models import CSPANdata
 
 val = datetime.now().date()
-api_key = "0j2wWr10819hVgK4DT8LSata-j82U84y9vafA4YX"
-api_url = f"https://api.c-spanarchives.org/2.0/mentions?query=speaker&limit=&personid=&date={val}&maxdate=&mindate=&page=&videotype=/get"
+api_key = "0j2wWr10819hVgK4DT8LSataj82U84 y9vafA4YX"
+api_url = f"https://api.c-spanarchives.org/2.0/mentions?query=speaker&limit=&personid=&date={val}&maxdate=&mindate=&page=&videotype=?apikey={api_key}"
 auth_ = HTTPBasicAuth('apikey', api_key)
-response = requests.get(api_url, headers={"Authentication": f"Basic {api_key}"})
+response = requests.get(api_url, headers=None)
 
 if response.status_code == 200:
     data = response.json()
